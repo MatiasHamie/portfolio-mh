9b9b4347b4565b4d7d55124f0e55b793c26119e2
MatiasHamie <mnhamie@gmail.com>
2021-02-02 00:42:48 -0300

    diseño de la pagina de contacto terminado

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,9 +1,35 @@
 import React from "react";
 
 const ContactPage = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>
-      <h1>Contact Page</h1>
+    <div className="contact-container">
+      <form onSubmit={handleSubmit}>
+        <p>Email</p>
+        <input type="email" name="email" placeholder="Ingrese Email" />
+        <p>Motivo</p>
+        <input type="texto" name="texto" placeholder="Ingrese Motivo" />
+        <p>Descripcion</p>
+        <textarea
+          name="desc-motivo"
+          id="desc-motivo"
+          cols="30"
+          rows="10"
+          placeholder="..."
+        ></textarea>
+        <button type="submit">
+          Enviar Consulta <i className="fas fa-paper-plane"></i>
+        </button>
+      </form>
+
+      <div className="contact-description">
+        <div className="contact_description animate__animated animate__slideInUp">
+          <h1 className="contact_title">Contacto</h1>
+        </div>
+      </div>
     </div>
   );
 };
