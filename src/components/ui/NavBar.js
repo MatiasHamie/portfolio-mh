b61be628cb2aefc261b8483efc9f9f19aa82ca22
MatiasHamie <mnhamie@gmail.com>
2021-02-08 13:45:38 -0300

    arregle navbar expandida responsive

--- src/components/ui/NavBar.js
+++ src/components/ui/NavBar.js
@@ -1,32 +1,42 @@
 import React, { useState } from "react";
+import { Navbar } from "react-bootstrap";
 import { NavLink, useHistory } from "react-router-dom";
 import logo from "../../assets/img/LogoMH.svg";
 
 const NavBar = () => {
-  const history = useHistory();
-
-  const handleLogoClick = () => {
-    history.push("/");
-  };
-
+  //estilo de un link activo
   const styleLinkActive = {
     fontWeight: "bold",
     color: "yellow",
     textShadow: "1 1 .8rem yellow",
   };
 
-  const [toggle, setToggle] = useState(false);
+  // navegacion logo
+  const history = useHistory();
 
-  const handleToggle = () => {
-    setToggle(!toggle);
+  const handleLogoClick = () => {
+    history.push("/");
   };
 
+  //colapsa el navbar cuando se clickea en un link
+  const handleToggle = () => {
+    setTimeout(() => {
+      setExpanded(false);
+    }, 500);
+  };
+
+  const [expanded, setExpanded] = useState(false);
+
   return (
-    <nav
+    <Navbar
+      className="navbar"
+      collapseOnSelect
+      expand="md"
+      variant="dark"
+      expanded={expanded}
       style={{ position: "relative", zIndex: 99 }}
-      className="navbar navbar-expand-lg navbar-dark vh-10 overflow-hidden"
     >
-      <div className="container-fluid">
+      <Navbar.Brand className="navbar-brand" href="#home">
         <img
           src={logo}
           alt="logo"
@@ -35,59 +45,52 @@ const NavBar = () => {
           className="navbar-brand mx-5"
           onClick={handleLogoClick}
         />
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          onClick={handleToggle}
-        >
-          {!toggle ? (
-            <i className="hamburger fas fa-bars"></i>
-          ) : (
-            <i className="hamburger fas fa-times"></i>
-          )}
-        </button>
-        <div
-          className="collapse navbar-collapse"
-          id="navbarNav"
-          data-target=".navbar-collapse"
-        >
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item styled-links mx-md-5">
-              <div className="nav-bar_link_line w-50 mx-auto"></div>
-              <NavLink
-                className="nav-link text-center"
-                activeStyle={styleLinkActive}
-                to="/portfolio"
-              >
-                <b>P</b>ortafolio
-              </NavLink>
-            </li>
-            <li className="nav-item styled-links mx-md-5">
-              <div className="nav-bar_link_line w-50 mx-auto"></div>
-              <NavLink
-                className="nav-link text-center"
-                to="/about"
-                activeStyle={styleLinkActive}
-              >
-                <b>S</b>obre Mi
-              </NavLink>
-            </li>
-            <li className="nav-item styled-links  mx-md-5">
-              <div className="nav-bar_link_line w-50 mx-auto"></div>
-              <NavLink
-                className="nav-link text-center"
-                to="/contact"
-                activeStyle={styleLinkActive}
-              >
-                <b>C</b>ontactame
-              </NavLink>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+      </Navbar.Brand>
+      <Navbar.Toggle
+        onClick={() => setExpanded(expanded ? false : "expanded")}
+        aria-controls="responsive-navbar-nav"
+      >
+        {!expanded ? (
+          <i className="hamburger fas fa-bars"></i>
+        ) : (
+          <i className="hamburger fas fa-times"></i>
+        )}
+      </Navbar.Toggle>
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <ul className="navbar-nav ms-auto">
+          <li className="nav-item styled-links mx-md-5" onClick={handleToggle}>
+            <div className="nav-bar_link_line w-50 mx-auto"></div>
+            <NavLink
+              className="nav-link text-center"
+              activeStyle={styleLinkActive}
+              to="/portfolio"
+            >
+              <b>P</b>ortafolio
+            </NavLink>
+          </li>
+          <li className="nav-item styled-links mx-md-5" onClick={handleToggle}>
+            <div className="nav-bar_link_line w-50 mx-auto"></div>
+            <NavLink
+              className="nav-link text-center"
+              to="/about"
+              activeStyle={styleLinkActive}
+            >
+              <b>S</b>obre Mi
+            </NavLink>
+          </li>
+          <li className="nav-item styled-links  mx-md-5" onClick={handleToggle}>
+            <div className="nav-bar_link_line w-50 mx-auto"></div>
+            <NavLink
+              className="nav-link text-center"
+              to="/contact"
+              activeStyle={styleLinkActive}
+            >
+              <b>C</b>ontactame
+            </NavLink>
+          </li>
+        </ul>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
 
